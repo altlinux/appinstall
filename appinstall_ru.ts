372422642da20bdead8f1baebf2d813cc0d02d8c
Andrey Cherepanov <cas@altlinux.org>
2022-05-15 19:10:03 +0300

    Update for eepm 3.18.6

--- appinstall_ru.ts
+++ appinstall_ru.ts
@@ -101,58 +101,59 @@
     </message>
     <message>
         <location filename="strings.cpp" line="12"/>
+        <source>Duplicati from the official site</source>
+        <translation>Duplicati c официального сайта</translation>
+    </message>
+    <message>
+        <location filename="strings.cpp" line="13"/>
         <source>EAGLE (EDA software) from the official site</source>
         <oldsource>Install EAGLE (EDA software) from the official site</oldsource>
         <translation>EAGLE с официального сайта EDA software</translation>
     </message>
     <message>
-        <location filename="strings.cpp" line="13"/>
+        <location filename="strings.cpp" line="14"/>
         <source>Microsoft Edge browser (dev) from the official site</source>
         <oldsource>Install Microsoft Edge browser (dev) from the official site</oldsource>
         <translation>Браузер Microsoft Edge с официального сайта</translation>
     </message>
     <message>
-        <location filename="strings.cpp" line="14"/>
+        <location filename="strings.cpp" line="15"/>
         <source>Geogebra 6 from the official site</source>
         <translation>Geogebra 6 с официального сайта</translation>
     </message>
     <message>
-        <location filename="strings.cpp" line="15"/>
         <source>Install glusterfs7 (or upgrade from glusterfs6)</source>
-        <translation>Установка glusterfs7 (или обновление с glusterfs6)</translation>
+        <translation type="vanished">Установка glusterfs7 (или обновление с glusterfs6)</translation>
     </message>
     <message>
-        <location filename="strings.cpp" line="16"/>
         <source>Install glusterfs8 (or upgrade from glusterfs7)</source>
-        <translation>Установка glusterfs8 (или обновление с glusterfs7)</translation>
+        <translation type="vanished">Установка glusterfs8 (или обновление с glusterfs7)</translation>
+    </message>
+    <message>
+        <source>Install glusterfs9 (or upgrade from glusterfs8)</source>
+        <translation type="vanished">Установка glusterfs9 (или обновление с glusterfs8)</translation>
     </message>
     <message>
         <location filename="strings.cpp" line="17"/>
-        <source>Install glusterfs9 (or upgrade from glusterfs8)</source>
-        <translation>Установка glusterfs9 (или обновление с glusterfs8)</translation>
-    </message>
-    <message>
-        <location filename="strings.cpp" line="18"/>
         <source>MS SQL Server 2019 from the official site</source>
         <translation>MS SQL Server 2019 с официального сайта</translation>
     </message>
     <message>
-        <location filename="strings.cpp" line="19"/>
+        <location filename="strings.cpp" line="18"/>
         <source>MS SQL Server sqlcmd and bcp from the official site</source>
         <translation>Программы sqlcmd и bcp с официального сайта MS SQL Server</translation>
     </message>
     <message>
-        <location filename="strings.cpp" line="24"/>
+        <location filename="strings.cpp" line="21"/>
         <source>R7 Office for Linux from the official site</source>
         <translation>R7 Office с официального сайта</translation>
     </message>
     <message>
-        <location filename="strings.cpp" line="35"/>
         <source>Install wine packages</source>
-        <translation>Установить пакеты WINE</translation>
+        <translation type="vanished">Установить пакеты WINE</translation>
     </message>
     <message>
-        <location filename="strings.cpp" line="36"/>
+        <location filename="strings.cpp" line="33"/>
         <source>XnView MP: Image management from the official site</source>
         <translation>Программа управления изображениями XnView MP с официального сайта</translation>
     </message>
@@ -169,25 +170,29 @@
         <translation type="vanished">Установка 32-битных пакетов wine-etersoft на 64-битную систему</translation>
     </message>
     <message>
-        <location filename="strings.cpp" line="23"/>
         <source>Remove all possible python2 packages</source>
         <oldsource>Install 32 bit wine packages on 64 bit system</oldsource>
-        <translation>Удаление всех возможных пакетов под python2</translation>
+        <translation type="vanished">Удаление всех возможных пакетов под python2</translation>
     </message>
     <message>
         <source>Install 32 bit wine-vanilla packages on 64 bit system</source>
         <translation type="vanished">Установка 32-битных пакетов wine-vanilla на 64-битную систему</translation>
     </message>
     <message>
-        <location filename="strings.cpp" line="26"/>
+        <location filename="strings.cpp" line="23"/>
         <source>Skype for Linux - Stable/Release Version from the official site</source>
         <oldsource>Install ONLYOFFICE for Linux from the official site</oldsource>
         <translation>Skype с официального сайта</translation>
     </message>
     <message>
-        <location filename="strings.cpp" line="21"/>
-        <source>Install php7 (or upgrade from php5)</source>
-        <translation>Установка PHP7 (или обновление с PHP5)</translation>
+        <location filename="strings.cpp" line="16"/>
+        <source>Контур.Толк (ktalk) from the official site</source>
+        <translation>Контур.Толк (ktalk) с официального сайта</translation>
+    </message>
+    <message>
+        <location filename="strings.cpp" line="19"/>
+        <source>Obsidian from the official site</source>
+        <translation>Obsidian с официального сайта</translation>
     </message>
     <message>
         <location filename="strings.cpp" line="20"/>
@@ -196,55 +201,56 @@
     </message>
     <message>
         <location filename="strings.cpp" line="22"/>
-        <source>Install php8 (or upgrade from php7)</source>
-        <translation>Установка PHP8 (или обновление с PHP7)</translation>
-    </message>
-    <message>
-        <location filename="strings.cpp" line="25"/>
         <source>Sferum for Linux from the official site</source>
+        <oldsource>Install php8 (or upgrade from php7)</oldsource>
         <translation>Сферум с официального сайта</translation>
     </message>
     <message>
-        <location filename="strings.cpp" line="27"/>
+        <location filename="strings.cpp" line="24"/>
         <source>Spotify client for Linux from the official site</source>
         <translation>Клиент Spotify с официального сайта</translation>
     </message>
     <message>
-        <location filename="strings.cpp" line="28"/>
+        <location filename="strings.cpp" line="25"/>
         <source>Sputnik browser from the official site</source>
         <translation>Спутник Браузер с официального сайта</translation>
     </message>
     <message>
-        <location filename="strings.cpp" line="29"/>
+        <location filename="strings.cpp" line="26"/>
+        <source>Sublime Text 4 from the official site</source>
+        <translation>Sublime Text 4 с официального сайта</translation>
+    </message>
+    <message>
+        <location filename="strings.cpp" line="27"/>
         <source>TamTam messenger from the official site</source>
         <translation>Мессенджер TamTam с официального сайта</translation>
     </message>
     <message>
-        <location filename="strings.cpp" line="30"/>
+        <location filename="strings.cpp" line="28"/>
         <source>Microsoft Teams for Linux from the official site</source>
         <oldsource>Install Skype for Linux - Stable/Release Version from the official site</oldsource>
         <translation>Microsoft Teams с официального сайта</translation>
     </message>
     <message>
-        <location filename="strings.cpp" line="31"/>
+        <location filename="strings.cpp" line="29"/>
         <source>Teamviewer from the official site</source>
         <oldsource>Install Spotify client for Linux from the official site</oldsource>
         <translation>Teamviewer с официального сайта</translation>
     </message>
     <message>
-        <location filename="strings.cpp" line="32"/>
+        <location filename="strings.cpp" line="30"/>
         <source>Telegram client from the official site</source>
         <oldsource>Install TamTam messenger from the official site</oldsource>
         <translation>Клиент Telegram с официального сайта</translation>
     </message>
     <message>
-        <location filename="strings.cpp" line="33"/>
+        <location filename="strings.cpp" line="31"/>
         <source>Viber for Linux from the official site</source>
         <oldsource>Install Microsoft Teams for Linux from the official site</oldsource>
         <translation>Клиент Viber с официального сайта</translation>
     </message>
     <message>
-        <location filename="strings.cpp" line="34"/>
+        <location filename="strings.cpp" line="32"/>
         <source>Vivaldi browser from the official site</source>
         <oldsource>Install Teamviewer from the official site</oldsource>
         <translation>Браузер Vivaldi с официального сайта</translation>
@@ -254,19 +260,19 @@
         <translation type="vanished">Установка клиента Telegram с официального сайта</translation>
     </message>
     <message>
-        <location filename="strings.cpp" line="37"/>
+        <location filename="strings.cpp" line="34"/>
         <source>Yandex browser from the official site</source>
         <oldsource>Install Viber for Linux from the official site</oldsource>
         <translation>Яндекс Браузер с официального сайта</translation>
     </message>
     <message>
-        <location filename="strings.cpp" line="38"/>
+        <location filename="strings.cpp" line="35"/>
         <source>Yandex Disk from the official site</source>
         <oldsource>Install Vivaldi browser from the official site</oldsource>
         <translation>Клиент Яндекс Диск с официального сайта</translation>
     </message>
     <message>
-        <location filename="strings.cpp" line="39"/>
+        <location filename="strings.cpp" line="36"/>
         <source>Zoom client from the official site</source>
         <oldsource>Install Yandex browser from the official site</oldsource>
         <translation>Клиент Zoom с официального сайта</translation>
